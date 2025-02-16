cbbabf048c05c20bf37d3613d763b6f683fbe241
Neurocoda <public@neurocoda.com>
2025-02-16 13:44:04 +0800

    Site updated: 2025-02-16 13:44:03

--- js/plugins/mermaid.js
+++ js/plugins/mermaid.js
@@ -1,1 +1,7 @@
-if(theme.plugins.mermaid.enable===true){try{swup.hooks.on("page:view",()=>{mermaid.initialize()})}catch(e){}}
+if (theme.plugins.mermaid.enable === true) {
+  try {
+    swup.hooks.on("page:view", () => {
+      mermaid.initialize();
+    });
+  } catch (e) {}
+}
